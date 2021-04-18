d2d095aee18dfc308647e57da0958144d8f89545
achmadwirra <achmad.wirra@gmail.com>
2021-04-18 12:32:09 +0800

    Operator If dan Else uang cukup

--- src/Percabangan/IfElse.java
+++ src/Percabangan/IfElse.java
@@ -3,7 +3,7 @@ package Percabangan;
 public class IfElse {
     public static void main(String[] args){
         double totalBelanja = 60000;
-        double uangDiDompet = 30000;
+        double uangDiDompet = 100000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi belanjaan anda");
